dff9dcbf063dffa965206e2f3322b1690b52c762
Chihui <NGCH0118@e.ntu.edu.sg>
2021-03-04 01:47:22 +0800

    Added JavaDoc for Ui class

--- src/main/java/ui/Ui.java
+++ src/main/java/ui/Ui.java
@@ -12,6 +12,9 @@ public class Ui {
     private final String ERROR_DIVIDER_LINE = "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!";
     public static final String FOUNDLINE ="FOUND!!!FOUND!!!FOUND!!!FOUND!!!";
 
+    /**
+     * Greets user when the program starts
+     */
     public static void printMenu() {
         System.out.println("Hello! Im Duke\n" + logo + "What can I do for you?");
         System.out.println("> Use the keyword todo");
@@ -24,9 +27,13 @@ public class Ui {
         System.out.println("Input here: ");
     }
 
+    /**
+     * Retrieves the userInput that will determine the command
+     * @return userInput
+     */
     public String getUserInput() {
         Scanner sc = new Scanner(System.in);
-        String userInput = sc.nextLine().trim().toLowerCase();
+        String userInput = sc.nextLine().trim();
         return userInput;
     }
 
@@ -34,6 +41,10 @@ public class Ui {
         System.out.println(ERROR_DIVIDER_LINE);
     }
 
+    /**
+     * Prints all error messages for errors encountered during the program runtime
+     * @param messages Error Messages to be printed
+     */
     public void printErrorMessage(String... messages) {
         printErrorDivider();
         print("Error Message: ");
@@ -43,17 +54,29 @@ public class Ui {
         printErrorDivider();
     }
 
+    /**
+     * To print any messages required
+     * @param messages Message to be printed
+     */
     public void printMessage(String ...messages){
         for(String message : messages){
             print(message);
         }
     }
 
+    /**
+     * To print single line message
+     * @param string string to print
+     */
     public void print(String string) {
         System.out.println(string);
     }
 
-
+    /**
+     * Ends the program when the user enters the command "bye"
+     * @param userInput userInput
+     * @return boolean value true if userInput contains "bye"
+     */
     public Boolean sayGoodBye(String userInput){
         if (userInput.contains("BYE")){
             printMessage("Bye Bye! See you Again");
